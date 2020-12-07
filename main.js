6d4784070696dc1a88004712d18fc643b19e310c
Shikhar-Joshi <shikharjoshi03@gmail.com>
2020-12-07 12:23:22 +0530

    Initial commit

--- main.js
+++ main.js
@@ -16,28 +16,53 @@ let allSongs = [
     },
     Music2 = {
         artist: 'Millind Gaba Ft Kamal Raja',
-        musicFile: 'songs/Nazar Lag Jayegi - Millind Gaba Ft Kamal Raja.mp3',
+        musicFile: 'songs/Nazar Lag Jayegi - Millind Gaba Ft Kamal Raja 320 Kbps.mp3',
         musicName: 'Nazar Lag Jayegi',
         artistPic: './pic/Millind_Gaba.jpg'
     }
-]
+];
 
 let counter = 0;
-audioPlayer.src = allSongs[counter].musicFile
-image.src = allSongs[counter].artistPic
-artistName.innerText = allSongs[counter].artist
-songName.innerText = allSongs[counter].musicName
+audioPlayer.src = allSongs[counter].musicFile;
+image.src = allSongs[counter].artistPic;
+artistName.innerText = allSongs[counter].artist;
+songName.innerText = allSongs[counter].musicName;
 
-playBtn.addEventListener('click', playSong)
-function playSong(){
-    console.log('Out')
+let playSong = () =>{
     if(audioPlayer.paused){
-        playBtn.innerHTML = '<em class="fas fa-pause"></em>'
-        console.log('Hello')
-        audioPlayer.play()
+        playBtn.innerHTML = '<em class="fas fa-pause"></em>';
+        audioPlayer.play();
     }
     else if(audioPlayer.play){
-        playBtn.innerHTML = '<em class="fas fa-play"></em>'
-        audioPlayer.pause()
+        playBtn.innerHTML = '<em class="fas fa-play"></em>';
+        audioPlayer.pause();
     }
 }
+playBtn.addEventListener('click', playSong)
+
+let nextSong = () =>{
+    counter ++;
+    if(counter > allSongs.length - 1)
+        counter = 0;
+    
+    audioPlayer.src = allSongs[counter].musicFile;
+    image.src = allSongs[counter].artistPic;
+    artistName.innerText = allSongs[counter].artist;
+    songName.innerText = allSongs[counter].musicName;
+    playBtn.innerHTML = '<em class="fas fa-pause"></em>';
+    audioPlayer.play();
+};
+nextBtn.addEventListener('click', nextSong)
+
+let prevSong = () =>{
+    counter --;
+    if(counter < 0)
+        counter = allSongs.length - 1;
+    audioPlayer.src = allSongs[counter].musicFile;
+    image.src = allSongs[counter].artistPic;
+    artistName.innerText = allSongs[counter].artist;
+    songName.innerText = allSongs[counter].musicName;
+    playBtn.innerHTML = '<em class="fas fa-pause"></em>';
+    audioPlayer.play();
+};
+prevBtn.addEventListener('click', prevSong)
